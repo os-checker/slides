c91a9d37cb7c777f276c695fda94827a889e3d9b
zjp <jiping_zhou@foxmail.com>
2025-07-28 15:41:59 +0000

    ci: deploy multilple slides with index.html

---
+++ gen.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+rm dist -rf
+mkdir dist
+
+md=(
+  ./src/2025-07-28_Pre-RFC_Safety-Property-System.md
+  # slides_init.md
+)
+
+site=(
+  Safety-Property-System
+  # demo
+)
+
+# build static pages
+for i in "${!md[@]}"; do
+  mkdir dist/${site[$i]}
+  npm run build -- ${md[$i]} --out dist/${site[$i]} --base ${BASE}
+done
+
+# generate index.html
+slides=$(
+  for i in "${!md[@]}"; do
+    ele=${md[$i]#./src/} # remove ./src/
+    ele=${ele/_/ }       # replace the first _ by space
+    ele=${ele%.md}       # remove .md
+    printf '  <li>  <a href="./%s"> %s </li>\\\n' "${site[$i]}" "${ele}"
+  done
+)
+sed -e '/^SLIDES$/c\'"${slides}" .github/index.html >dist/index.html
