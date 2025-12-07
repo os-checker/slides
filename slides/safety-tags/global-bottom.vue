2ca9f0923f40758c967e5aba41eb599b7d0303e4
zjp <jiping_zhou@foxmail.com>
2025-12-07 08:13:40 +0000

    safety-tags: add page numbers; adjust styling of inline code, h2 h3

---
+++ slides/safety-tags/global-bottom.vue
@@ -0,0 +1,6 @@
+<!-- an example footer for pages -->
+<template>
+  <footer v-if="$nav.currentLayout !== 'cover'" class="absolute bottom-0 right-4 p-2">
+    {{ $nav.currentPage }} / {{ $nav.total }}
+  </footer>
+</template>
