b9ad3e404b7c36e76d2c51a75e3ace58c1bc76e9
zjp <jiping_zhou@foxmail.com>
2025-12-12 03:48:42 +0000

    fix: remove vite.config.js and not mark @slidev/types external

--- slides/safety-tags/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['@slidev/types'], // 将 @slidev/types 标记为外部模块
-    },
-  },
-});
